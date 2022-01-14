4409803174cfba02b7c99d4ff334fbf0ef312048
shuvro111 <iamshuvrosarkar@gmail.com>
2022-01-15 02:15:58 +0600

    First Commit with Google O-Auth 🌄

---
+++ README.md
@@ -0,0 +1,21 @@
+---
+#### NextJS, Next Auth, Tailwind - Authentication &nbsp; :rocket:
+##### By [Shuvro Sarkar](https://github.com/shuvro111)
+---
+
+
+#### Tools Used &nbsp; :hammer_and_wrench:
+- [Next.js](https://nextjs.org/)
+- [TailwindCSS](https://tailwindcss.com/)
+- [NextAuth](https://nextjs.org/)
+
+
+___
+
+
+#### Usage :dart:
+
+   Feel free to fork or clone it for learning purpose.  :-)
+
+___
+#### Don't Forget To Star This Repo  &nbsp;:star:
---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,20 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+export default NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
+  ],
+  secret: process.env.SECRET,
+  debug: false,
+});
